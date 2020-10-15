800dcb0331531ecaea2a828c7e9e921ef13e5af0
Fabiano Pontes <fabiano.pontesc@gmail.com>
2020-10-15 19:40:45 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC";
+            ViewData["Email"] = "teste@gmail.com";
 
             return View();
         }
